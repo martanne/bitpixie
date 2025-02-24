3f841cbbcd01feb534e2fe8f15a9bd78bdc02ead
Marc André Tanner <mat@brain-dump.org>
2025-02-24 08:19:55 +0100

    Move initramfs generation into subfolder

--- pxe/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -f tftp/grubx64.efi ] && [ -f tftp/shimx64.efi ]; then
-	echo "Skipping Linux related artifacts download"
-else
-	docker run --platform linux/amd64 --rm -v "$PWD":/mnt -w /root \
-		alpine:3.20.3 "/mnt/download-linux.sh"
-fi
